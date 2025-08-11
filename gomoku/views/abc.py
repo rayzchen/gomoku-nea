9f29cca2a618fc7f8de48da9db256d4df81bfe44
Ray Chen <tankimarshal2@gmail.com>
2025-08-12 00:57:28 +0100

    Fix inheriting from InterfaceView

--- gomoku/views/abc.py
+++ gomoku/views/abc.py
@@ -1,9 +1,7 @@
 # Module imports
 from PySide6.QtWidgets import QWidget, QApplication
-from abc import ABCMeta, abstractmethod
 
-class InterfaceView(QWidget, metaclass=ABCMeta):
-    @abstractmethod
+class InterfaceView(QWidget):
     def reset(self):
         # Must be overridden in subclass
         # (may be empty as well)
